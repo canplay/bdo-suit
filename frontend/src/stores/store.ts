b42dfc1d479f5c034d70e73375dff46ca9085ec8
canplay <canplay@vip.qq.com>
2023-08-17 23:56:19 +0800

    #1

--- frontend/src/stores/store.ts
+++ frontend/src/stores/store.ts
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia';
 export const useStore = defineStore('global', {
   state: () => ({
     // backend: '/backend',
-    backend: 'http://1.13.22.82:51530',
+    backend: 'http://36.102.195.102:51530',
     // backend: 'http://127.0.0.1:51530',
     user: {
       characters: [] as any,
